fab201a22f0999e77d8b4a3a466fba3ea7657a0e
Ambre Gorias <ambre.gorias@gmail.com>
2022-02-24 15:35:59 -1000

    add tour guides referencing

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+// const User = require('./userModel'); // user for user embedding example
 // const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
@@ -102,6 +103,12 @@ const tourSchema = new mongoose.Schema(
         day: Number,
       },
     ],
+    guides: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User',
+      },
+    ],
   },
   // add virtual properties
   {
@@ -121,6 +128,13 @@ tourSchema.pre('save', function (next) {
   this.slug = slugify(this.name, { lower: true });
   next();
 });
+
+//  EXAMPLE OF DATA EMBEDDING
+// tourSchema.pre('save', async function (next) {
+//   const guidesPromises = this.guides.map(async (id) => await User.findById(id)); // array filled with promises
+//   this.guides = await Promise.all(guidesPromises); // overwrite promises with user documents
+//   next();
+// });
 
 // tourSchema.pre('save', function (next) {
 //   console.log('Will save document...');
@@ -144,11 +158,21 @@ tourSchema.pre(/^find/, function (next) {
   next();
 });
 
+// populate tours with guide documents
+tourSchema.pre(/^find/, function (next) {
+  this.populate({
+    path: 'guides',
+    select: '-__v -passwordChangedAt',
+  });
+  next();
+});
+
 tourSchema.post(/^find/, function (docs, next) {
   console.log(`Query took ${Date.now() - this.start} milliseconds`);
   next();
 });
 
+// EXPORT TOUR MODEL
 const Tour = mongoose.model('Tour', tourSchema);
 
 module.exports = Tour;
