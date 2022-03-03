6fb89bf1fe4664b8a782fa86386ed0dfdcd9e3e9
Ambre Gorias <ambre.gorias@gmail.com>
2022-03-02 21:56:07 -1000

    refactor common function in handlerFactory

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -3,14 +3,45 @@ const AppError = require('../utils/appError');
 
 exports.deleteOne = (Model) =>
   catchAsync(async (req, res, next) => {
-    const document = await Model.findByIdAndDelete(req.params.id);
+    const doc = await Model.findByIdAndDelete(req.params.id);
 
-    if (!document) {
+    if (!doc) {
       return next(new AppError('No document found with that ID', 404));
     }
 
     res.status(204).json({
       status: 'success',
       data: null,
+    });
+  });
+
+exports.updateOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!doc) {
+      return next(new AppError('No document found with that ID', 404));
+    }
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        data: doc,
+      },
+    });
+  });
+
+exports.createOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.create(req.body);
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        data: doc,
+      },
     });
   });
